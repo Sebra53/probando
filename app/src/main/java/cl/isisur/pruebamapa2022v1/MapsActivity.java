b18ac024c99ec4094e1a8ee6fe975eece388d59f
pablo <pablocalderoncornejo@gmail.com>
2024-09-13 12:14:21 -0300

    cambios 2024

--- app/src/main/java/cl/isisur/pruebamapa2022v1/MapsActivity.java
+++ app/src/main/java/cl/isisur/pruebamapa2022v1/MapsActivity.java
@@ -31,36 +31,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
         LatLng Unach00 = new LatLng(-36.638270, -71.997210);
         LatLng Unach01 = new LatLng(-36.637071, -71.995432);
         LatLng Unach02 = new LatLng(-36.635186, -71.996880);
         LatLng Unach03 = new LatLng(-36.589673,-72.082447);
 
-
-
-
-
-
         mMap.addMarker(new MarkerOptions().position(Unach00).title("Entrada Unach"));
         mMap.addMarker(new MarkerOptions().position(Unach01).title("LC1"));
         mMap.addMarker(new MarkerOptions().position(Unach02).title("Dormitorio de M"));
         mMap.addMarker(new MarkerOptions().position(Unach03).title("IPVG"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Unach03));
+
+        LatLng london = new LatLng(-36.589673,-72.082447);
+
+// Mover la cámara a la ubicación de Londres
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(london));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(london, 10));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(Unach03));
+
         mMap.setMinZoomPreference(10.0f);
         mMap.setMaxZoomPreference(50.0f);
 
